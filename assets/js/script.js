d51ce49bc50e1c38a2fd236ef313fd92bc7e5da6
Services0987 <smmservices0987@gmail.com>
2025-01-10 21:45:11 +0530

    Chef hyperlink

--- assets/js/script.js
+++ assets/js/script.js
@@ -1012,3 +1012,23 @@ document.addEventListener('click', function(event) {
     }
 });
 
+
+  function openModal() {
+    const modal = document.getElementById('chefModal');
+    modal.style.display = 'block';
+  }
+
+  function closeModal() {
+    const modal = document.getElementById('chefModal');
+    modal.style.display = 'none';
+  }
+
+
+  // Close the modal when clicking outside of it
+  window.onclick = function(event) {
+    const modal = document.getElementById('chefModal');
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  }
+
